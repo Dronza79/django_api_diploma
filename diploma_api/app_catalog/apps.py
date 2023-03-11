2e3b36d0dc55018bfdfe87d45998bd3aa4e08085
Zaycev Andrey <mr_zaycev@mail.ru>
2023-03-11 11:42:11 +0300

    добавил модель продуктов и изображений у ним

--- diploma_api/app_catalog/apps.py
+++ diploma_api/app_catalog/apps.py
@@ -5,4 +5,3 @@ class AppCatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_catalog'
     verbose_name = "Настройка каталога"
-    verbose_name_plural = "Изображение"
--- diploma_api/app_products/admin.py
+++ diploma_api/app_products/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, ImageProduct, PropertyProduct, TitleProperty
+
+
+def register_hidden_models(*model_names):
+    for model in model_names:
+        model_admin = type(
+            str(model)+'Admin', (admin.ModelAdmin,), {'get_model_perms': lambda self, request: {}})
+        admin.site.register(model, model_admin)
+
+
+class GalleryInline(admin.TabularInline):
+    model = ImageProduct
+    verbose_name = "Отображение"
+    verbose_name_plural = "Отображения"
+    extra = 0
+
+
+class PropertyInline(admin.TabularInline):
+    model = PropertyProduct
+    verbose_name = 'Характеристики'
+    verbose_name_plural = "Характеристики"
+    extra = 0
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    ordering = ['id']
+    list_per_page = 10
+    list_filter = ["category"]
+    list_display = ['id', 'title', 'category', 'price', 'count', 'slug', "date"]
+    list_display_links = ['title']
+    list_editable = ['price', 'count']
+    # readonly_fields = ['slug']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [
+        PropertyInline,
+        GalleryInline,
+    ]
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug',)
+        }),
+        ('Настройки продаж', {
+            'classes': 'wide',
+            'fields': (('price', 'count'),),
+        }),
+        ('Категория и описание', {
+            'classes': 'wide',
+            'fields': ('category', 'description', 'fullDescription',),
+        }),)
+
+
+register_hidden_models(TitleProperty,)
